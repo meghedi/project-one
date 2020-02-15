5e46eef596481a3b5da50c7e45742fa884b40bbf
Meghedi Hacopi <hacopian.m@gmail.com>
2020-02-15 11:53:51 -0800

    added zipcode and state validation

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -18,33 +18,51 @@ function getApiCallObj(url) {
 function getInputValue() {
   inputVal = $('#zipCodeInput').val();
   let weatherApiUrl = '';
-  if (isNaN(inputVal)){
-    weatherApiUrl = "https://api.weatherbit.io/v2.0/forecast/daily?city="+ inputVal +"&units=I&days=5&key=" + weatherApiKey;
-  }else{
-    inputVal = parseInt(inputVal);
-    weatherApiUrl = "https://api.weatherbit.io/v2.0/forecast/daily?&postal_code="+ inputVal +"&units=I&days=5&key=" + weatherApiKey;
+  if (isNaN(inputVal)) {
+    let inputArray = inputVal.split(",");
+    let cityName = inputArray[0];
+    let state = inputArray[1];
+
+    console.log(cityName , state);
+    if(typeof state ==='undefined'){
+      console.log('here');
+      $("#myModal").find("#modalTitle").text("City Name, State");
+      $("#myModal").find("#modalBodyText").text("Please Enter City, State");
+      $('#myModal').modal('show');
+      return;
+    }
+    weatherApiUrl = "https://api.weatherbit.io/v2.0/forecast/daily?city=" + cityName +","+ state +"&units=I&days=5&key=" + weatherApiKey;
+    getApiCallObj(weatherApiUrl);
+  } else {
+  //  inputVal = parseInt(inputVal);
+    if(/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(inputVal)) {
+      weatherApiUrl = "https://api.weatherbit.io/v2.0/forecast/daily?&postal_code=" + inputVal + "&units=I&days=5&key=" + weatherApiKey;
+      getApiCallObj(weatherApiUrl);
+    }else{
+      //alert("not zip code");
+      $("#myModal").find("#modalTitle").text("Zip Code");
+      $("#myModal").find("#modalBodyText").text("Please Enter a Zip Code.")
+      $('#myModal').modal('show');
+      return;
+    }
   }
 
- // let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + inputVal + "&appid=" + weatherApiKey
- getApiCallObj(weatherApiUrl);
+
 
   $.ajax(apiCall).then(function (result) {
     console.log(result);
-    /*for (let i = 0; i < result.weather.length; i++) {
-      console.log(result.weather[i].main);
-      let imgUrl = "http://openweathermap.org/img/w/" + result.weather[i].icon + ".png";
-      $('#weatherIcon').html('<img src="' + imgUrl + '">');
-      getGiphy(result.weather[i].main);
-      getwoied(result.coord.lon, result.coord.lat)
-    }*/
+
     $('#forecast .forFourDayForecast').empty();
     $('#giphyImg').empty();
     getGiphy(result.data[0].weather.description);
     $('.current-temperature__value').html(result.data[0].temp + "°");
     $('.current-temperature__summary').text(result.data[0].weather.description);
-    $('.current-temperature__icon').attr('src',  "https://www.weatherbit.io/static/img/icons/" + result.data[0].weather.icon + ".png")
+    $('.current-temperature__icon').attr('src', "https://www.weatherbit.io/static/img/icons/" + result.data[0].weather.icon + ".png");
 
-   for (let i = 1; i < result.data.length; i++) {
+    //getCurrentInfo(result.city_name);
+
+
+    for (let i = 1; i < result.data.length; i++) {
       let imgUrl = "https://www.weatherbit.io/static/img/icons/" + result.data[i].weather.icon + ".png";
       getTemprature(result.data[i].max_temp, result.data[i].low_temp, imgUrl);
     }
@@ -60,17 +78,39 @@ function getGiphy(main) {
     console.log(result);
     for (let i = 0; i < result.data.length; i++) {
       let imgUrl = result.data[i].images.fixed_width.url;
-      $('#giphyImg').append('<img src="' + imgUrl + '" id="boxGiphyImg">');
+      $('#giphyImg').append('<img src="' + imgUrl + '" id="boxGiphyImg"  alt="current weather giphy">');
 
     }
   });
 }
 
-function getTemprature(maxTemp, lowTemp, imgUrl){
+function getTemprature(maxTemp, lowTemp, imgUrl) {
 
- let newHtml = `<div class="col-sm-3" class="fourDayForecast">
-  <div class="boxSpace"><div>${maxTemp}° <span class="lineBetween"> | </span> ${lowTemp}° </div><div><img src="${imgUrl}"></div></div>`;
+  let newHtml = `<div class="col-sm-3" class="fourDayForecast">
+  <div class="boxSpace"><div>${maxTemp}° <span class="lineBetween"> | </span> ${lowTemp}° </div><div><img src="${imgUrl}" class="img-fluid" alt="4 day forecast"></div></div>`;
   $('#forecast .forFourDayForecast').append(newHtml);
+}
+
+function movingLetters() {
+  var textWrapper = document.querySelector('.ml2');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({ loop: true })
+    .add({
+      targets: '.ml2 .letter',
+      scale: [4, 1],
+      opacity: [0, 1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 950,
+      delay: (el, i) => 70 * i
+    }).add({
+      targets: '.ml2',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
 }
 
 
@@ -78,5 +118,11 @@ $('#go').on('click', function (event) {
   event.preventDefault();
   getInputValue();
 });
+
+$(document).ready(function () {
+  movingLetters();
+});
+
+
 
 
